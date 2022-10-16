cd2d1715b0d17310f6deb5c14c7dc6b5f2c883c1
zzlev <gpalektzja@gmail.com>
2022-10-16 16:51:20 +0900

    Refactor test codes.

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,30 +1,33 @@
 from chispa.dataframe_comparer import assert_df_equality
-from pyspark.sql import Row, SparkSession
+from pyspark.sql import DataFrame, Row, SparkSession
 from pyspark.sql import types as T
 from src import etl
 
 
 class TestETL:
-    def test(
+    def test_success(
         self,
         spark: SparkSession,
         input_schema: T.StructType,
         output_schema: T.StructType,
     ):
         # given
-        given, then = get_given_then_data()
-        src = spark.createDataFrame(data=given, schema=input_schema)
-
+        given_df, then_df = get_given_then_df(
+            spark=spark, input_schema=input_schema, output_schema=output_schema
+        )
         # when
-        result = etl.transform(src)
+        result = etl.transform(given_df)
 
         # then
-        dest = spark.createDataFrame(data=then, schema=output_schema)
-        assert_df_equality(result, dest, ignore_nullable=True)
+        assert_df_equality(result, then_df, ignore_nullable=True)
 
 
-def get_given_then_data() -> tuple[list[Row], list[Row]]:
+def get_given_then_df(
+    spark: SparkSession, input_schema: T.StructType, output_schema: T.StructType
+) -> tuple[DataFrame, DataFrame]:
     given = [Row(title="Test Spark", price=10, author="Hyemi Noh", rate=5.0)]
+    given_df = spark.createDataFrame(data=given, schema=input_schema)
+
     then = [
         Row(
             lower_title="test spark",
@@ -34,4 +37,5 @@ def get_given_then_data() -> tuple[list[Row], list[Row]]:
             rate_commen="very good",
         )
     ]
-    return given, then
+    then_df = spark.createDataFrame(data=then, schema=output_schema)
+    return given_df, then_df
